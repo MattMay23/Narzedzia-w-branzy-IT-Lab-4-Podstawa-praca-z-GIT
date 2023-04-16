9eecea2cf6a85f93d7e81711a5a9d3de901ee226
Mateusz Mnich <dsw51231@student.dsw.edu.pl>
2023-04-16 16:23:53 +0200

    zmiana421

--- skrypt.sh
+++ skrypt.sh
@@ -5,3 +5,21 @@ if [[ $1 == "--date" ]]; then
 else
     echo "Nieznana opcja"
 fi
+#!/bin/bash
+
+if ["$1" == "--logs" ]; then
+	if [ -z "$2" ]; then
+		pliczki=100
+	else
+		pliczki="$2"
+	fi
+	for ((i=1; i<=pliczki; i++)); do
+		nazwa_pliku=$(echo "log${i}.txt")
+		echo "Tworzenie pliku $nazwa_pliku"
+		echo "$nazwa_pliku" > "$nazwa_pliku"
+		echo "Stworzone przez skrypt.sh" >> "$nazwa_pliku"
+		echo "$(date)" >> "$nazwa_pliku"
+	done
+else
+	echo "$(date)"
+fi
